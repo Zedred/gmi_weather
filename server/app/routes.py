d8a960f50daff949ee183bed21228fd14d728e5b
Charles Wyman <chevwy@umich.edu>
2025-02-01 23:05:30 -0500

    Added LoginForm

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import flask_app
+from app.forms import LoginForm
 
 @flask_app.route('/')
 @flask_app.route('/index')
@@ -11,3 +12,10 @@ def index():
         {'date_time':'2024-10-12 12:30pm EST', 'TempC':21, 'TempF':72}]
     return render_template('index.html', title='Home', user=user, weather_data=data)
 
+@flask_app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for user {}, remember_me{}'.format(
+            form.username.data, form.remember_me.data))
+    return redirect(url_for('index'))
